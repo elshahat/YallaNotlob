bb02a9bff2373574e58893640320396be8181e90
AyaMagdyMohamed <aya.magdy948@yahoo.com>
2017-04-05 12:24:11 +0200

    latestOrders

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
+var mongoose=require("mongoose");
 var express= require("express");
+var fs=require("fs");
 var app=express();
 //........... Routers
 var autheRoutes=require("./controllers/auth");
 var homeRoutes=require("./controllers/home");
-
-<<<<<<< HEAD
-var OrderRoutes=require("./routes/orders.js")
-
-=======
->>>>>>> origin/master
-
-
+var OrderRoutes=require("./routes/orders")
 
 //................ Moddlewares
 app.use("/auth",autheRoutes);
 app.use("/home",homeRoutes);
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.use("/orders",OrderRoutes);
 
-=======
->>>>>>> origin/master
 
 //................ Views
 app.set('view engine','ejs');
 app.set('views','./views');
 
-<<<<<<< HEAD
 mongoose.connect("mongodb://127.0.0.1:27017/notlob");
 var files_arr=fs.readdirSync(__dirname+"/models")
 files_arr.forEach(function(file){
   require(__dirname+"/models/"+file);
 });
-=======
->>>>>>> origin/master
 
 app.listen(8030);
--- routes/orders.js
+++ routes/orders.js
@@ -1,18 +1,31 @@
 var express=require("express");
-// Load MongoDB Driver
-var mongoose=require("mongoose")
-
-var fs=require("fs");
-var app=express();
-
-app.use("/",HomeRoutes);
-app.set('view engine','ejs')
-app.set('views','./views')
-
-// open connection with mongodb
-// Server IP : Port Numner / Database Name..
+var mongoose=require("mongoose");
+var router=express.Router();
 
 
+router.get("/",function(req,resp){
+  /* Update
+  mongoose.model("users").update({},{},{},function(){
+
+  })*/
+  /* Remove
+  mongoose.model("users").remove({},function(){
+
+  })*/
+  email="a@iti.com"
+  mongoose.model("orders").find({"ownerEmail":"a@iti.com"},{"_id":false,"date":true},function(err,orders){
+    // mongoose.model("orders").populate(data,{path:"ownerEmail"},function(err,orders){
+    for(var i = 0; i <orders.length; i++) {
+       console.log(orders[i].date);
+             resp.json(orders[i].date);
+        }
+
+    // })
+    //resp.render("home/index",{"products_data":data});
+  })
+})
 
 
-app.listen(8090);
+
+
+module.exports=router;
