b88b24be3896aa43bc8f813312f459adceadff3c
sara-Hassaan <sara.1164@el-eng.menofia.edu.eg>
2017-04-18 19:18:01 +0300

    Add files via upload
    database.js ==> 'url' : 'mongodb://sarah@localhost:27017/notlob' 
    auth.js         ==> 'clientID' ,clientSecret','callbackURL'

---
+++ PassportDemo/config/auth.js
@@ -0,0 +1,22 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1141172965995171', // your App ID
+        'clientSecret'  : '490a3e234dd941212ab9139a4f16ffb7', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
---
+++ PassportDemo/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	'url' : 'mongodb://sarah@localhost:27017/notlob' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
